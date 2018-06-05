61710630e0c454c1b633e45442dfa30fa892332e
sirbardo <bardo_italiano@hotmail.it>
2018-06-05 10:50:52 +0200

    Added camera, and basic interface

--- App.js
+++ App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, Platform} from 'react-native';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Ciao!</Text>
-        <Text>Pene.</Text>
-        <Image 
-        style={{width:100, height:100}}
-        source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}></Image>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style = {styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            permissionDialogTitle={'Permission to use camera'}
+            permissionDialogMessage={'We need your permission to use your camera phone'}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center', }}>
+          <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style={styles.capture}
+          >
+            <Text>What's this food!?</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
+
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options)
+      console.log(data.uri);
+      console.log(data.base64);
+    }
+  };  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'black'
   },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  }
 });
+
